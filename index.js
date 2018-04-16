285529bd99b07609f6fa9bcf0e71ccadbed4236e
Unknown <timur.rabadji@gmail.com>
2018-04-16 15:08:26 +0300

    koa server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
+const logger =require('koa-logger');
 
+app.use(logger());
 // response
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+  ctx.body = 'Hello Vlad anton';
 });
 
 app.listen(3000);
