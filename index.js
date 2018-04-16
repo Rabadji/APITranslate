3e4c4e18c60b7025f4ed0275dcedcb969bd18723
Unknown <timur.rabadji@gmail.com>
2018-04-16 15:14:05 +0300

    ew

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
-const logger =require('koa-logger');
 
-app.use(logger());
 // response
 app.use(ctx => {
   ctx.body = 'Hello Vlad anton';
 });
 
 app.listen(3000);
+console.log("Ura server work");
